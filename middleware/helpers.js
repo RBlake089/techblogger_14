1315af0e3f24c9f311761ceca7852c1b2e772f7a
rblake089 <rblake089@gmail.com>
2023-08-08 14:43:36 -0400

    move helpers into utils

--- middleware/helpers.js
+++
@@ -1,7 +0,0 @@
-// Exported module containing a function for formatting dates
-module.exports = {
-  // Function to format a date in the MM/DD/YYYY format
-  formatDate: (date) => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
-  },
-};
