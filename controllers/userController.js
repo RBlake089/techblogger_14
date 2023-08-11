d186442f41a72439bcaba943cca4946eabb0bcbe
rblake089 <rblake089@gmail.com>
2023-08-11 09:54:17 -0400

    Made some changes for user stuff

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,86 +6,101 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
   // Method to render the signup page
-  getSignupPage: async (req, res) => {
+  getSignupPage: (req, res) => {
     res.render('signup');
   },
 
   // Method to render the login page
-  getLoginPage: async (req, res) => {
+  getLoginPage: (req, res) => {
     res.render('login');
   },
 
   // Method to create a new user (user registration)
-  register: async (req, res) => {
+  register: (req, res) => {
     const {
       body: { username, password },
     } = req;
-    try {
-      // Creating a new user record in the database with the provided credentials
-      const user = await User.create(req.body);
 
-      // Removing the password from the user object (for security) before saving it in the session
-      delete user.password;
+    User.create(req.body)
+      .then(user => {
+        delete user.password;
 
-      // Saving the user details in the session and marking the user as logged in
-      req.session.save(() => {
-        req.session.loggedIn = true;
-        req.session.username = user.username;
-        req.session.user_id = user.user_id;
-        res.status(200).json(user);
+        req.session.save()
+          .then(() => {
+            req.session.loggedIn = true;
+            req.session.username = user.username;
+            req.session.user_id = user.user_id;
+            res.status(200).json(user);
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
       });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
   },
 
   // Method to handle user login
-  login: async (req, res) => {
+  login: (req, res) => {
     const {
       body: { username, password },
     } = req;
-    try {
-      // Checking if the provided username exists in the database
-      const user = await User.findOne({ where: { username } });
-      if (!user) {
-        return res.status(401).json({ message: 'Invalid credentials' });
-      }
 
-      // Comparing the provided password with the stored hashed password using bcrypt
-      const pwMatch = await bcrypt.compare(password, user.password);
-      if (!pwMatch) {
-        return res.status(401).json({ message: 'Invalid credentials' });
-      }
+    User.findOne({ where: { username } })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({ message: 'Invalid credentials' });
+        }
 
-      // Saving the user details in the session and marking the user as logged in
-      req.session.loggedIn = true;
-      req.session.username = user.username;
-      req.session.user_id = user.user_id;
-      await req.session.save();
+        bcrypt.compare(password, user.password)
+          .then(pwMatch => {
+            if (!pwMatch) {
+              return res.status(401).json({ message: 'Invalid credentials' });
+            }
 
-      // Sending a response indicating successful login
-      res.status(200).json({
-        user: username,
-        user_id: req.session.user_id,
-        loggedIn: req.session.loggedIn,
-        message: 'You are now logged in!',
+            req.session.loggedIn = true;
+            req.session.username = user.username;
+            req.session.user_id = user.user_id;
+            req.session.save()
+              .then(() => {
+                res.status(200).json({
+                  user: username,
+                  user_id: req.session.user_id,
+                  loggedIn: req.session.loggedIn,
+                  message: 'You are now logged in!',
+                });
+              })
+              .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+              });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
       });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
   },
 
   // Method to handle user logout
   logout: (req, res) => {
-    // Destroying the user's session when logging out
     if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
+      req.session.destroy()
+        .then(() => {
+          res.status(204).end();
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
     } else {
-      // If the user is not logged in, respond with a 404 status
       res.status(404).end();
     }
   },
